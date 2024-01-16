fcede34c3aa8478ee8108829d97660c7dd1fa8c6
Adhnan00 <136177172+Adhnan00@users.noreply.github.com>
2024-01-16 21:02:09 +0530

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,9 +1,1 @@
-# simple_interest.py
 
-principal = float(input("Enter the principal amount: "))
-rate = float(input("Enter the annual interest rate (as a decimal): "))
-time = float(input("Enter the time the money is invested or borrowed for (in years): "))
-
-simple_interest = principal * rate * time
-
-print(f"Simple Interest: {simple_interest}")
